c8aa1cc9a20d4bd56c0c817b64f66fe03e69e231
jshef <j.sheffer747@gmail.com>
2025-05-31 12:11:54 +0300

    Enhance menu system and project structure
    Updated .gitignore to exclude unnecessary files and added project references to establish dependencies between `Ex04.Menus.Test` and `Ex04.Menus.Events`. Refactored `Program.cs` to implement a new interactive menu system. Introduced `InputValidator`, `LeafItem`, `MainMenu`, `MenuItem`, and `SubMenu` classes to improve user input handling and menu navigation. Updated various configuration files to reflect changes in project paths and dependencies.

---
+++ B25 Ex04 JonathanSheffer 302345749 KerenStoller 212127898/Ex04 Jonathan 302345749 Keren 212127898/Ex04.Menus.Events/LeafItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex04.Menus.Events;
+
+public class LeafItem : MenuItem
+{
+    public event Action? Selected;
+    public LeafItem(string i_Title) : base(i_Title)
+    {
+    }
+
+    protected virtual void onSelected()
+    {
+        Selected?.Invoke();
+    }
+
+    public override void Activate()
+    {
+        Console.Clear();
+        onSelected();
+    }
+}
+
--- B25 Ex04 JonathanSheffer 302345749 KerenStoller 212127898/Ex04 Jonathan 302345749 Keren 212127898/Ex04.Menus.Test/Program.cs
+++ B25 Ex04 JonathanSheffer 302345749 KerenStoller 212127898/Ex04 Jonathan 302345749 Keren 212127898/Ex04.Menus.Test/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ex04.Menus;
+using Ex04.Menus.Events;
+
+public class program
+{
+   private static void printHello()
+    {
+        Console.WriteLine("Hello from printHello method!");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+    public static void Main(string[] args)
+    {
+        Ex04.Menus.Events.MainMenu mainMenu = new Ex04.Menus.Events.MainMenu("Main Menu");
+        Ex04.Menus.Events.SubMenu subMenu1 = new Ex04.Menus.Events.SubMenu("Sub Menu 1");
+        mainMenu.AddMenuItem(subMenu1);
+        Ex04.Menus.Events.LeafItem test1 = new LeafItem("test 1");
+        test1.Selected += new Action(printHello);
+        subMenu1.AddMenuItem(test1);
+        mainMenu.Show();
+    }
+}
